0de21e659cae17525d4e892abbc94a740384e71e
kutlutezmetalsanayi-bit <kutlutezmetalsanayi@gmail.com>
2026-08-18 08:42:34 +0300

    Android sürüm kodunu 3'e yükselt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     applicationId = "com.aistudio.aritakip.kzrv"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
+    versionCode = 3
     versionName = "1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
